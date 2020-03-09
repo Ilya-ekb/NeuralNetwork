7358f9e50f3f4a23fa37b5009b36dfaad588b089
Ilya-ekb <54031934+Ilya-ekb@users.noreply.github.com>
2020-03-09 10:44:30 +0500

    Add project files.

---
+++ Neuron.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetwork
+{
+    public class Neuron
+    {
+        public List<double> Weight { get; }
+        public NeuronType NeuronType { get; }
+        public double Output { get; private set; }
+
+        public Neuron(int inputCount, NeuronType type = NeuronType.Normal)
+        {
+
+        }
+    }
+}
